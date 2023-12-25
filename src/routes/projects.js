d69f11d081be5a113ce9db5049e652e0927436d3
Renan Franca <renanfranca.dev@gmail.com>
2023-12-25 10:54:05 -0300

    chore: test POST ok!

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -13,4 +13,25 @@ router.get("/", connectDatabase, async (req, res, next) => {
   }
 });
 
+/* POST projects */
+
+router.post("/", connectDatabase, async (req, res, next) => {
+  try {
+    let { type, title, description, teachs, imageUrl, deploy, repository } =
+      req.body;
+    const resDB = await SchemaProjects.create({
+      type,
+      title,
+      description,
+      teachs,
+      imageUrl,
+      deploy,
+      repository,
+    });
+    res.status(200).json(resDB);
+  } catch {
+    res.status(500).json("internal server error");
+  }
+});
+
 module.exports = router;
--- src/services/db.js
+++ src/services/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = async () => {
+const connectDatabase = async (req, res, next) => {
   console.log("connecting database");
 
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+    try {
+      next();
+    } catch {}
     return mongoose;
   } catch (error) {
     console.log("ERROR:", error);
