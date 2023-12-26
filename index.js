d59de514df67ed7ebfc0cc80e07a0fc95d6a5ad1
Renan Franca <renanfranca.dev@gmail.com>
2023-12-26 09:51:42 -0300

    fix: bug vercel deploy, views deleted

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const projectsRouter = require("./src/routes/projects");
 const app = express();
 require("dotenv").config();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
